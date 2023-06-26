a5e40fc535cf745dde8bb244383b86bf0d2851af
Hossein Taybi <seven.number73@gmail.com>
2023-06-26 17:00:03 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -133,6 +133,13 @@ class Dev(Configuration):
 
     USE_TZ = True
 
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = 'seven.number73@gmail.com'
+    EMAIL_HOST_PASSWORD = 'fsyqpavdvvguxkwn'
+
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -146,6 +153,47 @@ class Dev(Configuration):
 
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+    DJANGO_ADMINS="Hossein htt,seven.number73@gmail.com;hossein Tayebi,hossein.taybi@yahoo.com"
+
+    LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "filters": ["require_debug_false"],
+        },
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+    }
 
 class Prod(Dev):
     DEBUG = False
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,16 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import redirect
 from blog.forms import CommentForm
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
-    
+    logger.debug("Got %d posts", len(posts))
+
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     if request.user.is_active:
@@ -20,6 +24,9 @@ def post_detail(request, slug):
                 comment.content_object = post
                 comment.creator = request.user
                 comment.save()
+                logger.info(
+    "Created comment on Post %d for user %s", post.pk, request.user
+)
                 return redirect(request.path_info)
         else:
             comment_form = CommentForm()
