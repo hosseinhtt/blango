5b35e0117e70632647fc0e7b18ba74553605fd52
Hossein Taybi <seven.number73@gmail.com>
2023-06-27 07:27:44 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -100,7 +100,12 @@ class Dev(Configuration):
         default=f"sqlite:///{BASE_DIR}/alternative_db.sqlite3",
     ),
     }
-
+    # PASSWORD_HASHERS = [
+    #   'django.contrib.auth.hashers.Argon2PasswordHasher',
+    #   'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    #   'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    #   'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    # ]
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
